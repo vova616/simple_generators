06cdda64cf5b2110b20d42b86e7994be6ea70550
Vova <vova616@gmail.com>
2020-08-28 21:23:04 +0300

    fix

--- adapter/src/lib.rs
+++ adapter/src/lib.rs
@@ -10,7 +10,7 @@ impl<G: Generator + Unpin> Iterator for GeneratorIteratorAdapter<G>
     type Item = G::Yield;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match { Pin::new(&mut self.0).resume() } {
+        match { Pin::new(&mut self.0).resume(()) } {
             GeneratorState::Yielded(x) => Some(x),
             GeneratorState::Complete(_) => None,
         }
